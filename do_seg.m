2aa29d6a26be7236497a3b6b09671161952d2f45
akanazawa <kanazawa@umiacs.umd.edu>
2012-01-24 17:55:02 -0500

    start do_seg

--- do_seg.m
+++ do_seg.m
@@ -1,12 +1,16 @@
 function do_seg(config)
 %%%%%%%%%%
-% do_train.m
-% blah
+% do_seg.m
+% Supersegments all N images in IMG_DIR and saves them in SEG_FILE where 
+% the data is in a N x 1 cell where each cell has the structure:
+%     file_name: the original img file name in absolute path
+%     segmented: H x W matrix where each pixel is assigned a label
+%                belonging to a region
+%     truth_file: path to the ground truth data (in txt)
 %
-% the data is in format:
-% 
-% saved in MODEL_FILE from config
 %%%%%%%%%%
 
 %% Evaluate global configuration file and load parameters
 eval(config);
+
+%% Load images
