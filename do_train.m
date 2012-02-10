1f7469c58a495e3b5115c223964f45dc1904bf97
guest <gues@mishraka-desktop.(none)>
2012-02-09 21:39:28 -0500

    do train almost finished?

--- do_train.m
+++ do_train.m
@@ -1,64 +1,46 @@
-function do_train(config)
+function do_train(config_file)
 %%%%%%%%%%
 % do_train.m
-% blah
-%
-% the data is in format:
+% This function is a simple implementation of iterative collective
+% classification using libsvm. We iteratively train on our data, make
+% predictions on the data, and append local neighborhood information onto
+% the features. 
 % 
 % saved in MODEL_FILE from config
 %%%%%%%%%%
 
 %% Evaluate global configuration file and load parameters
-eval(config);
-function [Model acc] = do_CollectiveTrain(config_file)
-% G - The data with the graph structure since we know the true label
-% G.data - feature values and label.
-% G.graph - Incidence graoh based on the label.
-% G.trueLabel - True Label of the data.
-% G.labelStack - The stack of estimated labels.
-% K - The level of stacks
-% MulticlassTrain - A function handle that takes in input and gives a classfiers
-% In this work we hv used libsvm as the classifier
- 
-% Model - The model obtained after training for different numbers of stack.
- 
-% Since we have the estimated label stack already intialized in G we need not do it again.
-% We will keep updating G as the feature stack so Dmc in the algorithm is
-% our G
 
-dummyData = double(G.data);
-[Num dim] = size(dummyData);
-G.data = cell(K,1);
-G.data{1,1} = dummyData;
-clear dummyData;
-Llab = length(unique(G.trueLabel));
+eval(config_file);
 
-% TODO: Lots of matrix concatenation this may not be very efficient since
-% matlab will supposedly reallocate memory a lot.
+load(TRAINING_DATA); % this will load 'data'
 
-load(SEG_FILE); % this will load 'data'
+numImgs = numel(data); % Number of images
+accuracy = zeros(TRAIN.K,3);
+models = cell(TRAIN.K,1);
 
-predictions
-accuracy = zeros(K,3);
-models = cell(K,1);
+% We need to stack data so that it can be sent to libsvm 
+ground_truth = zeros(numImgs * SEG.nC, 1);
+stacked_data = zeros(numImgs * SEG.nC, size(data{1}.feat, 2));
 
-% We need to stack data!
-ground_truth = zeros(I * SEG.nC, 1);
-stacked_data = zeros(I * SEG.nC, D);
-
-for img_ind = 1:I
+for img_ind = 1:numImgs
     base_index = (img_ind - 1) * SEG.nC;
     
-    stacked_data(((base_index+1):(base_index+SEG.nC)), : = data{img_ind}.feat
+    ground_truth(((base_index+1):(base_index+SEG.nC)), :) = data{img_ind}.label;
+    stacked_data(((base_index+1):(base_index+SEG.nC)), :) = data{img_ind}.feat;
 end
 
-for k = 1:K
+for k = 1:TRAIN.K
+    fprintf('Running stack level %d\n', k);
+    
     models{k} = svmtrain(ground_truth, stacked_data, libsvm_options);
     [predictions accuracy(k,:) decision_values] = svmpredict(ground_truth, stacked_data, models{k});
     
-    new_feats = [];
+    fprintf('Prediction accuracy: %g\n', accuracy(k,1));
     
-    for img_ind = 1:I
+    new_feats = zeros(numImgs * SEG.nC, size(CLASSES,2));
+    
+    for img_ind = 1:numImgs
         % get the predictions for the regions in this image and
         % element-wise multiply with the adjacency matrix
         % this replaces the binary values of the adjacency matrix
@@ -69,7 +51,7 @@ for k = 1:K
         
         % Get the frequencies for each class, for each region
         % and append it onto the new features
-        new_feats = [new_feats; hist(neigh_labels, CLASSES)'];
+        new_feats(((base_index+1):(base_index+SEG.nC)), :) = hist(neigh_labels, CLASSES)';
     end
     
     % Stack new neighborhood prediction information onto the features
@@ -77,4 +59,4 @@ for k = 1:K
 
 end
 
-
+save(MODEL_FILE, 'models', 'accuracy');
